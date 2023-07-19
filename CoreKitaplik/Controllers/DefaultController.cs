7efcf7ab5efba8428676535c1e5c157bbffacef1
merveakca <smkirac@gmail.com>
2023-07-19 17:25:29 +0300

    Add project files.

---
+++ CoreKitaplik/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreKitaplik.Models;
+
+namespace CoreKitaplik.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ktp = new List<Kitaplar>()
+            {
+                new Kitaplar() { ID = 1, KitapAd = "Satranç", Yazar = "Zweig" },
+                new Kitaplar() { ID = 2, KitapAd = "Dr Ox'un Deneyi", Yazar = "Jules Verne" },
+                new Kitaplar() { ID = 3, KitapAd = "Delifişek", Yazar = "Vasconceulos" },
+                new Kitaplar() { ID = 4, KitapAd = "Kırmızı Biziklet", Yazar = "Muzaffer İzgü" },
+                new Kitaplar() { ID = 5, KitapAd = "Toros Canavarı", Yazar = "Aziz Nesin" }
+            };
+            return View(ktp);
+        }
+        public IActionResult Deneme()
+        {
+            return View();
+        }
+        public IActionResult StatikTema()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreKitaplik/ViewComponents/YeniKitaplar.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreKitaplik.Models;
+
+namespace CoreKitaplik.ViewComponents
+{
+    public class YeniKitaplar: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var kitaplistesi = new List<Kitaplar>
+            {
+                new Kitaplar
+                {
+                    ID=6,
+                    KitapAd="Korku",
+                    Yazar="Zweig"
+                },
+                new Kitaplar
+                {
+                    ID=7,
+                    KitapAd="Anna Karanina",
+                    Yazar="Tolstoy"
+                }
+            };
+            return View(kitaplistesi);
+        }
+    }
+}
